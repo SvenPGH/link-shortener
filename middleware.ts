ebdedec74da0d6f1bd451322b425aedc479e497b
Puite Sven <puitesven+github@gmail.com>
2025-07-13 19:20:40 +0200

    Fix auth

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,10 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config'; // Import ONLY the edge-safe config
+
+// Initialize NextAuth using only the edge-safe config.
+// This creates an `auth` function that is compatible with the Edge Runtime.
+export default NextAuth(authConfig).auth;
+
 export const config = {
   // Use the matcher to specify which routes the middleware should protect.
   matcher: ['/my-links/:path*', '/profile/:path*'],
