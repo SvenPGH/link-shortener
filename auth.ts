49e3b976d59d8be84f51826a91f19856b6fe111b
Puite Sven <puitesven+github@gmail.com>
2025-07-13 17:33:33 +0200

    Fix Auth for Edge Runtime (Force Node Runtime)

--- auth.ts
+++ auth.ts
@@ -1,12 +1,47 @@
-import NextAuth from 'next-auth'
-import Google from 'next-auth/providers/google'
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import { authConfig } from './auth.config'; // Import the edge-safe config
 
-export const { auth } = NextAuth({
-  session: { strategy: 'jwt' },
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-})
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  ...authConfig,
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  callbacks: {
+    ...authConfig.callbacks,
+
+    // These callbacks use the database and will only run in the Node.js runtime
+    async session({ session, token }) {
+        const customToken = token as any;
+
+        if (customToken.sub && session.user) {
+            session.user.id = customToken.sub;
+            session.user.darkMode = customToken.darkMode;
+            session.user.emailNotifications = customToken.emailNotifications;
+            session.user.linkAnalytics = customToken.linkAnalytics;
+        }
+        return session;
+    },
+    async jwt({ token, user, trigger }) {
+        if (user) {
+            const dbUser = await prisma.user.findUnique({ where: { id: user.id }});
+            if(dbUser) {
+                token.sub = dbUser.id;
+                token.darkMode = dbUser.darkMode;
+                token.emailNotifications = dbUser.emailNotifications;
+                token.linkAnalytics = dbUser.linkAnalytics;
+            }
+        }
+        if (trigger === "update" && token.sub) {
+            const dbUser = await prisma.user.findUnique({ where: { id: token.sub as string } });
+            if (dbUser) {
+               token.name = dbUser.name;
+               token.darkMode = dbUser.darkMode;
+               token.emailNotifications = dbUser.emailNotifications;
+               token.linkAnalytics = dbUser.linkAnalytics;
+            }
+        }
+        return token;
+    }
+  }
+});
