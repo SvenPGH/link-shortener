1767eb12a41e465bf132bbb70bf8dc00999b3d20
Puite Sven <puitesven+github@gmail.com>
2025-07-13 18:27:40 +0200

    Fix auth

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,19 @@
-import {PrismaClient} from '@prisma/client/edge';
-import {withAccelerate} from '@prisma/extension-accelerate';
+import {PrismaClient} from '@prisma/client';
+import {PrismaNeon} from '@prisma/adapter-neon';
+import {Pool} from '@neondatabase/serverless';
 
 const prismaClientSingleton = () => {
-    return new PrismaClient().$extends(withAccelerate());
+    const neon = new Pool({connectionString: process.env.SVP_GL_DATABASE_URL});
+    const adapter = new PrismaNeon(neon);
+    return new PrismaClient({adapter});
 };
 
 declare global {
     var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
+
 export const prisma = globalThis.prisma ?? prismaClientSingleton();
+
 if (process.env.NODE_ENV !== 'production') {
     globalThis.prisma = prisma;
 }
