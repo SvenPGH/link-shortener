23bde57e5541fa301fe4602bce75ac832088511b
Puite Sven <puitesven+github@gmail.com>
2025-07-13 17:55:54 +0200

    Retry auth edge fix

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,8 @@ export default auth;
 // The matcher specifies which routes the middleware will protect.
 // This is more efficient than running it on every single request.
 export const config = {
-  matcher: ["/my-links/:path*", "/profile/:path*"],
+  matcher: [
+    "/my-links/:path*",
+    "/profile/:path*"
+  ],
 };
