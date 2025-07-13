c251001b14078c7522f64951e83ae66edba58cd0
Puite Sven <puitesven+github@gmail.com>
2025-07-13 18:14:34 +0200

    Fix auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,21 +1,18 @@
-import type { NextAuthConfig } from 'next-auth';
+import type {NextAuthConfig} from 'next-auth';
 
 export const authConfig = {
-  providers: [],
-  pages: {
-    signIn: '/auth/login',
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isProtectedRoute = nextUrl.pathname.startsWith('/my-links') || nextUrl.pathname.startsWith('/profile');
+    pages: {signIn: '/auth/login',},
+    callbacks: {
+        authorized({auth, request: {nextUrl}}) {
+            const isLoggedIn = !!auth?.user;
+            const isProtectedRoute = nextUrl.pathname.startsWith('/my-links') || nextUrl.pathname.startsWith('/profile');
 
-      if (isProtectedRoute) {
-        if (isLoggedIn) return true; // Allow access if logged in
-        return false; // Redirect unauthenticated users to the login page
-      }
-
-      return true; // Allow access to all other pages
+            if (isProtectedRoute) {
+                if (isLoggedIn) return true;
+                return false;
+            }
+            return true;
+        },
     },
-  },
+    providers: [],
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -1,10 +1,10 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { prisma } from '@/lib/prisma';
+import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
-import NextAuth from 'next-auth/next';
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
 import { authConfig } from './auth.config';
 
-export const { auth, handlers, signIn, signOut } = NextAuth(authConfig);
+export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
@@ -15,10 +15,9 @@ export const { auth, handlers, signIn, signOut } = NextAuth(authConfig);
     }),
   ],
   callbacks: {
-    // It's important to merge the callbacks, keeping the `authorized` callback from the config
-    ...authConfig.callbacks,
+    ...authConfig.callbacks, // Import the authorized callback
 
-    // These callbacks use the database and will only run on the server (Node.js runtime)
+    // These JWT/Session callbacks use the database and only run on the server
     async jwt({ token, user, trigger }) {
       if (user) {
         const dbUser = await prisma.user.findUnique({ where: { id: user.id } });
@@ -32,10 +31,10 @@ export const { auth, handlers, signIn, signOut } = NextAuth(authConfig);
       if (trigger === "update" && token.sub) {
         const dbUser = await prisma.user.findUnique({ where: { id: token.sub as string } });
         if (dbUser) {
-          token.name = dbUser.name;
-          token.darkMode = dbUser.darkMode;
-          token.emailNotifications = dbUser.emailNotifications;
-          token.linkAnalytics = dbUser.linkAnalytics;
+           token.name = dbUser.name;
+           token.darkMode = dbUser.darkMode;
+           token.emailNotifications = dbUser.emailNotifications;
+           token.linkAnalytics = dbUser.linkAnalytics;
         }
       }
       return token;
