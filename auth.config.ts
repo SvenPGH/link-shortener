22c23357bf0186de489ebe57251f97358c651c33
Puite Sven <puitesven+github@gmail.com>
2025-07-13 20:15:52 +0200

    Fix auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,15 +1,18 @@
+import type { Session } from 'next-auth';
+import type { NextRequest } from 'next/server';
+
 export const authConfig = {
     pages: {
         signIn: '/auth/login',
     },
     callbacks: {
-        authorized({auth, request: {nextUrl}}) {
+        authorized({ auth, request: { nextUrl } }: { auth: Session | null; request: NextRequest }) {
             const isLoggedIn = !!auth?.user;
             const isProtectedRoute = nextUrl.pathname.startsWith('/my-links') || nextUrl.pathname.startsWith('/profile');
 
             if (isProtectedRoute) {
                 if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to the login page
+                return false;
             }
             return true;
         },
